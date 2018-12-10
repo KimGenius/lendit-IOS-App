d73911f2212c9ef5955521985235a2cf9e577bba
GeniusK <geniusk1047@naver.com>
2018-12-11 08:21:19 +0900

    first commit

---
+++ lenditIOSApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  lenditIOSApp
+//
+//  Created by 김영재 on 10/12/2018.
+//  Copyright © 2018 GeniusK. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://invest.lendit.co.kr/invest")!
+        webView.load(URLRequest(url: url))
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+}
